4ba0585a4a0a61baae74d93bad79195ed06dc29b
kyosu-1 <sho013039@gmail.com>
2023-01-12 21:00:18 +0900

    add store.go

---
+++ internal/log/store.go
@@ -0,0 +1,94 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+// newStore creates a new store from a file.
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		buf:  bufio.NewWriter(f),
+		size: size,
+	}, nil
+}
+
+// Append append a new record to the store.
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	n = uint64(len(p))
+	err = binary.Write(s.buf, enc, n)
+	if err != nil {
+		return 0, 0, err
+	}
+	_, err = s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	err = s.buf.Flush()
+	if err != nil {
+		return 0, 0, err
+	}
+	s.size += lenWidth + n
+	return n, pos, nil
+}
+
+func (s *store) Read(pos uint64) (p []byte, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func (s *store) ReadAt(p []byte, off int64) (n int, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return err
+	}
+	return s.File.Close()
+}
